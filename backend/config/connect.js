a0ae57a574ae37eb6033361b6f1e42a71c7d0fd6
Kerston2104 <113289902+Kerston2104@users.noreply.github.com>
2024-04-09 18:58:52 +0530

    vercel file added

--- backend/config/connect.js
+++ backend/config/connect.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost:27017/collaboratales')
+const { MongoClient } = require('mongodb');
+
+
+// const uri = "mongodb+srv://admin:admin123@cluster0.ke2ab4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+// const client = new MongoClient(uri)
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.ke2ab4e.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(
     () => {
         console.log('connection established');
